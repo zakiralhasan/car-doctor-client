53507b0fe574195232b8b27b5209f21c11cfb880
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-02 12:06:32 +0600

    create authContext and added firebase auth system

---
+++ src/Contexts/AuthProvider.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createContext } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { useState } from "react";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const authInfo = { user };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
